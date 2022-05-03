fed37b676aad4b92dd89aeeafd0327c7708be456
hiepit <mvhiepit@gmail.com>
2022-05-03 12:07:34 +0700

    db and admin panel

--- yt_study/base/urls.py
+++ yt_study/base/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Home, name='home'),
-    path('room/<str:pk>/', views.Room, name='room'),
+    path('', views.home, name='home'),
+    path('room/<str:pk>/', views.room, name='room'),
 
 ]
--- yt_study/base/views.py
+++ yt_study/base/views.py
@@ -1,25 +1,21 @@
 from django.shortcuts import render
+from .models import Room
 
-rooms = [
-    {'id': 1, 'name': 'python'},
-    {'id': 2, 'name': 'java'},
-    {'id': 3, 'name': 'c'},
-    {'id': 4, 'name': 'c++'},
-    {'id': 5, 'name': 'js'},
+# rooms = [
+#     {'id': 1, 'name': 'python'},
+#     {'id': 2, 'name': 'java'},
+#     {'id': 3, 'name': 'c'},
+#     {'id': 4, 'name': 'c++'},
+#     {'id': 5, 'name': 'js'},
+# ]
 
-]
-
-def Home(request):
+def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 
-def Room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-            break
-
+def room(request, pk):
+    room = Room.objects.get(id = pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
