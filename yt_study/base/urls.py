f44b661a46f9d04c6a960247150b4a035fd8daf0
hiepit <mvhiepit@gmail.com>
2022-05-03 11:01:03 +0700

    templates

--- yt_study/base/urls.py
+++ yt_study/base/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Home, name = 'home'),
-    path('room/', views.Room, name = 'room'),
+    path('', views.Home, name='home'),
+    path('room/<str:pk>/', views.Room, name='room'),
 
 ]
--- yt_study/base/views.py
+++ yt_study/base/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
-def Home(request):
-    return render(request, 'base/home.html')
+rooms = [
+    {'id': 1, 'name': 'python'},
+    {'id': 2, 'name': 'java'},
+    {'id': 3, 'name': 'c'},
+    {'id': 4, 'name': 'c++'},
+    {'id': 5, 'name': 'js'},
 
-def Room(request):
-    return render(request , 'base/room.html')
+]
+
+def Home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+
+def Room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+            break
+
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
